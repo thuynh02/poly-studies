69059136d28d7156eb21694bf29fe79729ad6d77
Tony Huynh <th1142@nyu.edu>
2014-03-26 23:54:54 -0400

    SQL export updated.

--- product/createDatabase.sql
+++ product/createDatabase.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 26, 2014 at 04:09 AM
+-- Generation Time: Mar 27, 2014 at 04:53 AM
 -- Server version: 5.5.33
 -- PHP Version: 5.5.3
 
@@ -13,19 +13,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `demo`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_uploads`
---
-
-CREATE TABLE `user_uploads` (
-  `upload_id` int(11) NOT NULL AUTO_INCREMENT,
-  `image_name` text,
-  `created` int(11) DEFAULT NULL,
-  PRIMARY KEY (`upload_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 -- --------------------------------------------------------
 
@@ -42,3 +29,26 @@ CREATE TABLE `users` (
   UNIQUE KEY `username` (`username`),
   UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_uploads`
+--
+
+CREATE TABLE `user_uploads` (
+  `upload_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_name` text,
+  `created` int(11) DEFAULT NULL,
+  PRIMARY KEY (`upload_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- Dumping data for table `user_uploads`
+--
+
+INSERT INTO `user_uploads` (`upload_id`, `image_name`, `created`) VALUES
+(1, '1395884429ipad-mini.jpg', 1395884429),
+(2, '1395884447coke.jpg', 1395884447),
+(3, '1395884451twitter.jpg', 1395884451),
+(7, '1395888755Screen Shot 2014-02-16 at 6.10.29 PM.png', 1395888755);
