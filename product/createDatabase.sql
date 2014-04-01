c7dc82e9af0b309579dc2bc4a5609e4e020af508
Tony Huynh <th1142@nyu.edu>
2014-03-31 23:32:55 -0400

    Updating image_descriptions and feedback from pressing update has been added. createDatabase.sql has also been updated to match the current version.

--- product/createDatabase.sql
+++ product/createDatabase.sql
@@ -3,12 +3,18 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 27, 2014 at 04:53 AM
+-- Generation Time: Apr 01, 2014 at 05:30 AM
 -- Server version: 5.5.33
 -- PHP Version: 5.5.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `demo`
@@ -20,7 +26,8 @@ SET time_zone = "+00:00";
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(100) DEFAULT NULL,
@@ -36,20 +43,25 @@ CREATE TABLE `users` (
 -- Table structure for table `user_uploads`
 --
 
-CREATE TABLE `user_uploads` (
+DROP TABLE IF EXISTS `user_uploads`;
+CREATE TABLE IF NOT EXISTS `user_uploads` (
   `upload_id` int(11) NOT NULL AUTO_INCREMENT,
   `image_name` text,
   `image_path` text NOT NULL,
   `image_description` text,
   `created` int(11) DEFAULT NULL,
   PRIMARY KEY (`upload_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `user_uploads`
 --
 
-INSERT INTO `user_uploads` (`upload_id`, `image_name`, `image_description`, `image_path`, `created`) VALUES
-(1, '', '', '1395884429ipad-mini.jpg', 1395884429),
-(2, '', '', '1395884447coke.jpg', 1395884447),
-(3, '', '', '1395884451twitter.jpg', 1395884451),
+INSERT INTO `user_uploads` (`upload_id`, `image_name`, `image_path`, `image_description`, `created`) VALUES
+(4, 'Coca-Cola', '1395978215coke.jpg', 'Have you had Coca-Cola before?', 1395978215),
+(5, 'iPad-Mini', '1395978215ipad-mini.jpg', 'Have you used an iPad-Mini?', 1395978215),
+(6, 'Twitter', '1395978215twitter.jpg', 'Have you used Twitter?', 1395978215);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
