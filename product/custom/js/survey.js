9814629a3537adb89d90f4b77fe5c1026a17746f
Kay <kaytran@nyu.edu>
2014-04-10 17:04:54 -0400

    Implemented Warning and working on disabling submit.

--- product/custom/js/survey.js
+++ product/custom/js/survey.js
@@ -1,15 +1,5 @@
-// Validate is used to verify that all survey questions have been answered.
-// 
-// NOTE: A bool can be passed in order to enable alert messages. 
-//		 Alerts will tell the user what questions were left unanswered.
-// 
-// This function is used in survey.html. Form name, "initialSurvey" calls this function from attribute, 'onsubmit'
-// TO BE IMPLEMENTED: Check for valid age and if awareness questions are answered correctly
-// Ex: 	Question 11 asks that the user selects choice 2. 
-//		We need to ensure that choice is selected before moving on.
-function validate( showMessage ){
 
-	if( showMessage == null ){ showMessage = false; }
+function validateFilled( warn ){
 
 	$( ".question" ).removeClass( "has-errored" );
 	$( ".question" ).removeClass( "has-error" );
@@ -19,13 +9,13 @@ function validate( showMessage ){
 	// Check if age was given 
 	// ***MUST CHECK FOR NUMERICAL ANSWER UNDER 99***
 	if( $('input[name="age"]')[0].value == '' ){
-		$('#age').addClass( "has-error" );
+		if( warn ) $('#age').addClass( "has-error" );
 		isValid = false;
 	}
 
 	// Check if gender is given
 	if( $('input[name="gender"]:checked').length == 0 ){
-		$('#gender').addClass( "has-error" );
+		if( warn ) $('#gender').addClass( "has-error" );
 		isValid = false;
 	}
 
@@ -33,13 +23,13 @@ function validate( showMessage ){
 	// ***MUST CHECK FOR QUESTION 11. ANSWER MUST BE CHOICE 2***
 	var numOfLikertQuestions = 18;
 	var currentQuestion;
-	for( var i = 0; i < numOfLikertQuestions; ++i ){
+	for( var i = 1; i <= numOfLikertQuestions; ++i ){
 
 		// Likert scale (radio button input) questions
 		if( $('input[name="q' + i + '"]').attr("type") == 'radio' 
 			&& $('input[name="q' + i + '"]:checked').length == 0 ){
      		// message += 'Please select one option for question ' + i + '\n';
-     		$('#q' + i ).addClass( "has-errored" );
+     		if( warn ) $('#q' + i ).addClass( "has-errored" );
      		isValid = false;
 
 		}
@@ -48,10 +38,40 @@ function validate( showMessage ){
 		else if( $('input[name="q' + i + '"]').attr("type") == 'text' 
 				 && $('input[name="q' + i + '"]')[0].value == '' ){
 			// message += 'Please fill out question ' + i + '\n';
-			$('#q' + i ).addClass( "has-errored" );
+			if( warn ) $('#q' + i ).addClass( "has-errored" );
 			isValid = false;
 		}
 	}
+
+	//If, by the end of all the tests, nothing triggers an invalid answer, the answers are okay to go
+	return isValid;
+}
+
+// Validate is used to verify that all survey questions have been answered.
+// 
+// NOTE: A bool can be passed in order to enable alert messages. 
+//		 Alerts will tell the user what questions were left unanswered.
+// 
+// This function is used in survey.html. Form name, "initialSurvey" calls this function from attribute, 'onsubmit'
+// TO BE IMPLEMENTED: Check for valid age and if awareness questions are answered correctly
+// Ex: 	Question 11 asks that the user selects choice 2. 
+//		We need to ensure that choice is selected before moving on.
+function validateInput( ){
+
+	var isValid = validateFilled( true );
+
+	if( $('input[name="q5"]').val() != 6 ) { 
+		$('#q5' ).addClass( "has-errored" );
+		isValid = false; 
+	}
+	if ( $('input[name="q11"]').val() != 2 ) { 
+		$('#q11' ).addClass( "has-errored" );
+		isValid = false; 
+	}
+	if( $('input[name="q18"]').val() != "az" ) { 
+		isValid = false; 
+	}
+
 
 	//
 	// if( showMessage && message != ''){
@@ -61,6 +81,7 @@ function validate( showMessage ){
 	//If, by the end of all the tests, nothing triggers an invalid answer, the answers are okay to go
 	return isValid;
 }
+
 
 var surveyQuestions = [];
 var ratingQuestions = [];
@@ -93,16 +114,16 @@ function getQuestionData(){
 }
 
 function generateSurveyHTML( n, question ){
-	return '<tr class="question" id="q' + n + '"> \
+	return '<tr class="question" id="q' + ( n + 1 ) + '"> \
 				<td>' + ( n + 1 ) + '.</td> \
 				<td>' + question + '</td> \
-				<td><input type="radio" name="q' + n + '" value="1">1</td> \
-				<td><input type="radio" name="q' + n + '" value="2">2</td> \
-				<td><input type="radio" name="q' + n + '" value="3">3</td> \
-				<td><input type="radio" name="q' + n + '" value="4">4</td> \
-				<td><input type="radio" name="q' + n + '" value="5">5</td> \
-				<td><input type="radio" name="q' + n + '" value="6">6</td> \
-				<td><input type="radio" name="q' + n + '" value="7">7</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="1">1</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="2">2</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="3">3</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="4">4</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="5">5</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="6">6</td> \
+				<td><input type="radio" name="q' + ( n + 1 ) + '" value="7">7</td> \
 			</tr>';
 
 }
@@ -117,15 +138,7 @@ window.onload = function(){
 		$( '#scaleSurvey' ).append( generateSurveyHTML( i, surveyQuestions[i] ) )
 	};
 
-	// for (var i = 0; i < surveyQuestions.length; i++) {
-	// 	$('input[name="q' + i + '"][value=1]').attr("checked",true);
-	// };
-
-	// $('input[name="age"]').val( 18 );
-	// $('input[name="gender"][value="male"]').attr("checked",true);
-	// $('input[name="q4"][value=6]').attr("checked",true);
-	// $('input[name="q10"][value=2]').attr("checked",true);
-	// $('input[name="q18"]').val( "az" );
+	// $('#surveySubmit').addClass( "disabled" );
 
 	// Once the user clicks submit, begin to fill the survey data object with the answers
 	// Afterwards, convert the object into a JSON string 
@@ -133,7 +146,7 @@ window.onload = function(){
 
 		// Validate takes a boolean parameter. If true is passed, alerts appear, telling the user what
 		// questions were left unanswered
-		if( validate(true) ){
+		if( validateInput() ){
 			//For every checked radio button and provided text input, add the value to the survey data
 			$('input:checked, input[type="text"]').each( function(){
 				surveyData[$(this)[0].name] = $(this)[0].value;
@@ -162,20 +175,19 @@ window.onload = function(){
 
 	$('#fillAll').click( function(){
 		
-
 		for (var i = 0; i < surveyQuestions.length; i++) {
-			$('input[name="q' + i + '"][value=1]').attr("checked",true);
+			$('input[name="q' + (i + 1) + '"][value=1]').attr("checked",true);
 		};
 
 		$('input[name="age"]').val( 18 );
 		$('input[name="gender"][value="male"]').attr("checked",true);
-		$('input[name="q4"][value=6]').attr("checked",true);
-		$('input[name="q10"][value=2]').attr("checked",true);
+		$('input[name="q5"][value=6]').attr("checked",true);
+		$('input[name="q11"][value=2]').attr("checked",true);
 		$('input[name="q18"]').val( "az" );
 
 	});
 
-	$("#initialSurvey").submit( function(){
+	$("#initialSurvey").submit( function(e){
 		return false;
 	});
 }
