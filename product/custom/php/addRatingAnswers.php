115939ba181dcb90a23e49403e67e1299760c70a
Tony Huynh <th1142@nyu.edu>
2014-04-14 11:38:02 -0400

    Removed the parsing stage of updating ratingAnswers. Would have made it too complicated later down the road

--- product/custom/php/addRatingAnswers.php
+++ product/custom/php/addRatingAnswers.php
@@ -3,29 +3,8 @@
 
 	if( isset($_POST) ){
 
-		//Decode the json string and store it as an array of product answers
-		$answersArr = json_decode($_POST['productAnswers']);
-		$ratingAnswers = array();
-
-		//Iterate through each product answer and insert into the database
-		foreach( $answersArr as $productAnswer ){
-
-			$productName = htmlspecialchars(strip_tags($productAnswer->productName));
-			$questionTypes = $productAnswer->questionTypes;
-			$questionValues = $productAnswer->questionValues;
-
-			for( $i = 0; $i < count($questionTypes); $i++ ) {
-
-				$questionTypes[$i] = htmlspecialchars(strip_tags($questionTypes[$i]));
-				$questionValues[$i] = htmlspecialchars(strip_tags($questionValues[$i]));
-
-				array_push( $ratingAnswers, '{'.$productName.'_'.$questionTypes[$i].':'.$questionValues[$i].'}' );
-
-			}
-		}
-
 		$time = time();
-		$jsonRatingAnswers = json_encode($ratingAnswers);
+		$jsonRatingAnswers = $_POST['productAnswers'];
 
 		//**NOTE** Using a dummy user_id of -1 for now! 
 		$addAnswers = "UPDATE answers 
@@ -33,7 +12,7 @@
 		               WHERE user_id=-1 AND survey_id=1";
         mysqli_query( $bd, $addAnswers );
 		
-		print_r( json_encode($ratingAnswers) );
+		print_r( $jsonRatingAnswers );
 	}
 
 ?>
