6f36c888ea80347f9d11e3e5673ad505e44cc774
Tony Huynh <th1142@nyu.edu>
2014-04-24 17:29:00 -0400

    Testing if git ignore works

--- product/custom/php/db.php
+++ product/custom/php/db.php
@@ -1,8 +1,8 @@
 <?php
 $mysql_hostname = "localhost";
 $mysql_user = "root";
-$mysql_password = "BAgowan13sql";
-$mysql_database = "study";
+$mysql_password = "WRONG PASSWORD";
+$mysql_database = "demo";
 $prefix = "";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
 mysqli_select_db( $bd, $mysql_database) or die("Opps some thing went wrong");
